eb6650bf60e956909824c3630968c65ee5a50d66
Osvaldo Garcia <osvaldsilent@gmail.com>
2025-07-15 16:23:03 -0600

    fixing issues from sonar report#3

--- src/main/java/com/example/authservice/config/CorsConfig.java
+++ src/main/java/com/example/authservice/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.authservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,12 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
+    @Value("${app.cors.allowed-origin:*}")
     private String allowedOrigin;
-
-    public CorsConfig(String allowedOrigin) {
-        this.allowedOrigin = allowedOrigin;
-    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
